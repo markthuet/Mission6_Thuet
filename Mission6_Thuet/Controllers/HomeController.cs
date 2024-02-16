4a6bc49c9ca87ee15b8878f79bdb8a388466f254
markjamesthuet <markj2it@gmail.com>
2024-02-15 17:03:26 -0700

    css changes

--- Mission6_Thuet/Controllers/HomeController.cs
+++ Mission6_Thuet/Controllers/HomeController.cs
@@ -4,46 +4,54 @@ using System.Diagnostics;
 
 namespace Mission6_Thuet.Controllers
 {
+    // HomeController class definition
     public class HomeController : Controller
     {
-
+        // Private field to store MovieContext instance
         private MovieContext _movieContext;
-        public HomeController(MovieContext name) 
 
+        // Constructor for HomeController with MovieContext parameter
+        public HomeController(MovieContext name)
         {
             _movieContext = name;
         }
 
+        // Action method for the Index view
         public IActionResult Index()
         {
             return View();
         }
 
+        // Action method for the GetToKnow view
         public IActionResult GetToKnow()
         {
             return View();
         }
 
+        // HTTP GET action method for the movieApplication view
         [HttpGet]
         public IActionResult movieApplication()
         {
             return View();
         }
 
+        // HTTP POST action method for processing movieApplication form submission
         [HttpPost]
         public IActionResult movieApplication(Movies response)
         {
+            // Check if ModelState is not valid
             if (!ModelState.IsValid)
             {
                 // There are validation errors, return to the form with errors
                 return View(response);
             }
 
-            _movieContext.Movies.Add(response); // Add record to database
+            // Add the movie record to the database
+            _movieContext.Movies.Add(response);
             _movieContext.SaveChanges();
 
+            // Redirect to the confirmation view with the movie response
             return View("confirmation", response);
         }
-
     }
 }
